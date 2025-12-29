e605d9caeaf18ccc2ac1fcb87371be65ebda0d24
Aira <airadozik@gmail.com>
2025-12-29 15:27:37 +0500

    Added more variables

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // #region DOM_variables
 const content = document.querySelector(".content");
-
+const modal = document.querySelector("dialog");
+const show_modal = document.querySelector("button.show");
 //#endregion
