ec4c466296906fe9017e82071d3c655e64b625f8
Aira <airadozik@gmail.com>
2025-12-29 15:40:50 +0500

    added 2 IIEFI's to get template and handle modal

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-// #region DOM_variables
+// #region DOM variables
 const content = document.querySelector(".content");
-const modal = document.querySelector("dialog");
+const dialog = document.querySelector("dialog");
 const show_modal = document.querySelector("button.show");
 //#endregion
+
+const template = (() => {
+    function get(temp) {
+        if (!temp && typeof temp !== "string") return;
+        const template_node = document.querySelector(temp);
+        const clone = template_node.content.cloneNode(true);
+        return clone;
+    }
+
+    return { get };
+})();
+const modal = (() => {
+    const temp = template.get("#modal");
+
+    function show(e) {
+        e.addEventListener("click", () => {
+            dialog.showModal();
+        });
+    }
+    function close(e) {
+        e.addEventListener("click", () => {
+            dialog.close();
+        });
+    }
+
+    return { show, close };
+})();
